98ec5d10645e3a528056c5014de804b3ca576152
Khurram Saeed <kssuper007@gmail.com>
2021-04-22 17:11:10 +0500

    final changes creating a new timer app called timer02 link https://www.youtube.com/watch?v=4Zbf-PS9Q84 not done yet.

---
+++ lib/count_down_timer/timer02.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class second_timer extends StatefulWidget {
+  @override
+  _second_timerState createState() => _second_timerState();
+}
+
+class _second_timerState extends State<second_timer> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scafold(
+        
+      ),
+    );
+  }
+}
